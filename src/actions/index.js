c6f623cb53b9ba2212ad7bedccc706379146c499
Sailor Winkelman <sailorwinkelman@gmail.com>
2018-02-25 23:38:14 -0800

    Add receiveSong test, action type, action in lyricChange reducer, parse data from second API response (lyrics) and dispatch receiveSong to update state with new song.

--- src/actions/index.js
+++ src/actions/index.js
@@ -12,9 +12,33 @@ export function fetchSongId(title) {
       response => response.json(),
       error => console.log('An error occurred.', error)
     ).then(function(json) {
-      console.log('CHECK OUT THIS SWEET API RESPONSE!!!: ', json);
+      if (json.message.body.track_list.length > 0) {
+        const musicMatchId = json.message.body.track_list[0].track.track_id;
+        const artist = json.message.body.track_list[0].track.artist_name;
+        const title = json.message.body.track_list[0].track.track_name;
+        console.log('WOW! an API response!');
+        fetchLyrics(title, artist, musicMatchId, localSongId, dispatch);
+      } else {
+        console.log('We couldn\'t locate a song under that ID');
+      }
     });
   };
+}
+
+export function fetchLyrics(title, artist, musicMatchId, localSongId, dispatch){
+  return fetch('http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=' + musicMatchId + '&apikey=86bd7d90f10da3ddcca488342fe9426f').then(
+    response => response.json(),
+    error => console.log('An error occurred.', error)
+  ).then(function(json) {
+    if(json.message.body.lyrics) {
+      let lyrics = json.message.body.lyrics.lyrics_body;
+      lyrics = lyrics.replace('"', '');
+      const songArray = lyrics.split(/\n/g).filter(entry => entry!='');
+      dispatch(receiveSong(title, artist, localSongId, songArray));
+    } else {
+      console.log('We couldn\'t locate lyrics for that song!');
+    }
+  });
 }
 
 export const nextLyric = (currentSongId) => ({
@@ -36,4 +60,13 @@ export const requestSong = (title, localSongId) => ({
   type: types.REQUEST_SONG,
   title,
   songId: localSongId
+});
+
+export const receiveSong = (title, artist, songId, songArray) => ({
+  type: types.RECEIVE_SONG,
+  songId,
+  title,
+  artist,
+  songArray,
+  receivedAt: Date.now()
 });
